d3fecb8d5613bab5292496bfd7c40d5266c690b5
Randall Clayborn <claybear39@yahoo.com>
2016-05-14 20:00:32 -0700

    Initial Commit

---
+++ SceneEditorCookBookTests/SceneEditorCookBookTests.swift
@@ -0,0 +1,36 @@
+//
+//  SceneEditorCookBookTests.swift
+//  SceneEditorCookBookTests
+//
+//  Created by Randall Clayborn on 5/14/16.
+//  Copyright © 2016 claybear39. All rights reserved.
+//
+
+import XCTest
+@testable import SceneEditorCookBook
+
+class SceneEditorCookBookTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measureBlock {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
